d7b3a474f71ae5b0cd7551210e109a54393f54de
Morade B <morade.baghloul@gmail.com>
2024-05-12 15:39:45 +0200

    ajout affichage des catégories et filtrage des catégories

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,10 +1,11 @@
 
 
-//  l’appel à l’API avec fetch
+//  l’appel à l’API avec fetch pour recuperer les données du backend 
 
 async function getWorks() {
   const response = await fetch("http://localhost:5678/api/works");
-  const responseJson = await response.json();
+  return await response.json();
+
 }
 getWorks();
 
@@ -17,14 +18,60 @@ const gallery = document.querySelector(".gallery");
 
 async function affichageWorks() {
   const works = await getWorks();
-  // supprimez du html les travaus déja existant 
+
+  // supprimez du html les travaux déja existant 
 
   gallery.innerHTML = "";
+ 
 }
 affichageWorks();
 
 
 
+// affichage des boutons par categories
+
+// récupérer les categories et les afficher 
+
+const filters = document.querySelector(".filters")
+
+async function getCategorys() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  return await response.json();
+ }
+ 
+ async function CategorysButtons() {
+  const categorys = await getCategorys();
+  categorys.forEach((category) => {
+      const btn = document.createElement("button");
+      btn.textContent = category.name;
+      btn.id = category.id;
+      filters.appendChild(btn);
+  });
+}
+CategorysButtons();
+
+// filter au click sur le bouton par catégorie
+
+async function filterCategory() {
+  const images = await getWorks();
+  const buttons = document.querySelectorAll(".filters button");
+  buttons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        btnId = e.target.id;
+        gallery.innerHTML = "";
+          if (btnId !== "0") {
+            const triImages = images.filter((work) => {
+              return work.categoryId == btnId;
+             });
+             
+        }else {
+          affichageWorks();
+        }
+      });
+  });
+}
+
+filterCategory();
 
 
 
